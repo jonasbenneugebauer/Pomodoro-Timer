18a76cdc7c53c86bb581d6cda26e47fc14c2b99e
Snasn3n <jonasneugebauer.01@gmx.de>
2026-02-28 17:55:24 +0100

    console logging working

--- pomo/src/Pomodoro.java
+++ pomo/src/Pomodoro.java
@@ -1,11 +1,12 @@
 public class Pomodoro {
    private int totalSeconds;
+   private int minutes;
 
 
     public Pomodoro (int seconds, int minutes){
         this.totalSeconds = seconds + minutes * 60;
+        this.minutes = totalSeconds / 60;
     }
-
 public void start(){
 
     while(totalSeconds >0){
@@ -16,9 +17,17 @@ public void start(){
             e.printStackTrace();
         }
         totalSeconds--;
+        int mins = totalSeconds / 60;
+        int secs = totalSeconds % 60;
+        System.out.println(String.format("%02d:%02d", mins, secs));
 
     }
+    System.out.println("Pomodoro session ended.");
 }
 
-    
+public static void main(String[] args) {
+    Pomodoro pomodoro = new Pomodoro(20, 1);
+    pomodoro.start();
+}
+
 }
