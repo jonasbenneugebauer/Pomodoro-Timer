d10429107bae7a284a0604320a1343eee9f51d43
Snasn3n <jonasneugebauer.01@gmx.de>
2026-02-28 20:36:37 +0100

    added working frame

--- pomo/src/PomodoroGUI.java
+++ pomo/src/PomodoroGUI.java
@@ -1,3 +1,27 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class PomodoroGUI {
+
+    private JFrame frame;
+    private JLabel label;
+    private JButton startButton;
+    private JButton stopButton;
+
+    public PomodoroGUI() {
+        frame = new JFrame();   
+        label = new JLabel();
+        startButton = new JButton("Start");
+        stopButton = new JButton("Stop");
+        frame.setVisible(true);
+        frame.setSize(1080, 720);
+    }
+
+    public static void main(String[] args) {
+        new PomodoroGUI();
+    }
+    
+    
     
 }
