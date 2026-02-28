1e48b3cc10223ee55012babefcea881fe45e592b
Snasn3n <jonasneugebauer.01@gmx.de>
2026-02-28 17:36:19 +0100

    first start method implemented

--- pomo/src/Pomodoro.java
+++ pomo/src/Pomodoro.java
@@ -1,3 +1,24 @@
 public class Pomodoro {
+   private int totalSeconds;
+
+
+    public Pomodoro (int seconds, int minutes){
+        this.totalSeconds = seconds + minutes * 60;
+    }
+
+public void start(){
+
+    while(totalSeconds >0){
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        totalSeconds--;
+
+    }
+}
+
     
 }
