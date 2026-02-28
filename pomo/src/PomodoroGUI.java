9879f9cd4dee1cced980de2723cb357e0a6fd7e0
Snasn3n <jonasneugebauer.01@gmx.de>
2026-02-28 20:59:17 +0100

    added spacing and Buttons/labels

--- pomo/src/PomodoroGUI.java
+++ pomo/src/PomodoroGUI.java
@@ -1,6 +1,10 @@
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class PomodoroGUI {
 
@@ -8,15 +12,31 @@ public class PomodoroGUI {
     private JLabel label;
     private JButton startButton;
     private JButton stopButton;
+    private JPanel panel;
 
     public PomodoroGUI() {
-        frame = new JFrame();   
-        label = new JLabel();
+        frame = new JFrame("Pomodoro Timer"); 
+        label = new JLabel("00:00");
         startButton = new JButton("Start");
         stopButton = new JButton("Stop");
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        stopButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel.add(Box.createVerticalGlue()); // Add some space at the top
+        panel.add(label);
+        panel.add(Box.createVerticalStrut(20)); // Add some space between label and buttons
+        panel.add(startButton);
+        panel.add(Box.createVerticalStrut(10)); // Add some space between buttons
+        panel.add(stopButton);
+        panel.add(Box.createVerticalGlue()); // Add some space at the bottom
+        frame.add(panel);
         frame.setVisible(true);
         frame.setSize(1080, 720);
-    }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    } 
 
     public static void main(String[] args) {
         new PomodoroGUI();
